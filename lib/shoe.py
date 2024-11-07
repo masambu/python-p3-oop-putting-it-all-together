a11b13452438c8a9803ec0cd6b8e2394a5f00e1a
Kelvin Masambu <kevohmasambu@gmail.com>
2024-11-07 22:47:26 +0300

    done

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,28 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand 
+        self.size = size 
+
+    @property 
+    def size(self):
+        '''size property'''
+        return self._size
+
+    @size.setter 
+    def size(self, size):
+        '''size must be an integer'''
+        if isinstance(size, int):
+            self._size = size 
+        else:
+            print('size must be an integer')
+        self.condition = ''
+
+    def cobble(self):
+        print('Your shoe is as good as new!')
+        self.condition = 'New'
+
+
+adidas = Shoe('Adidas', 8)
+adidas.cobble()
